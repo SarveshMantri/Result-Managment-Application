5546595e73b226ee406acec3990edcdcd15123c9
SARVESH <sarveshmantri200@gmail.com>
2023-05-19 08:50:36 +0530

    Modified MYSQL connections

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const {v4: uuidv4} = require('uuid');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 // register view engine
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
-app.use( bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //session
-app.use(session({
+app.use(
+  session({
     secret: uuidv4(),
     resave: false,
-    saveUninitialized: true
-}));
-
+    saveUninitialized: true,
+  })
+);
 
 app.get("/", (req, res) => {
-    res.render('index');
+  res.render("index");
 });
 
 // Configuring Routes
 const studentRoutes = require("./routes/studentRoutes");
 const teacherRoutes = require("./routes/teacherRoutes");
-app.use("/student",studentRoutes);
-app.use("/teacher",teacherRoutes);
-
+app.use("/student", studentRoutes);
+app.use("/teacher", teacherRoutes);
 
 // Connection to Mysql
-const db = require('./connection.js');
+const db = require("./connection.js");
 
-db.connect((err) =>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log('Mysql Connected');
-    }
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Mysql Connected");
+  }
 });
-
 
 // create DB
 // app.get('/createdb', (req, res) =>{
@@ -53,13 +51,13 @@ db.connect((err) =>{
 //         res.send('Database created');
 //     });
 // });
+var port = process.env.PORT || 3000;
 
-
-app.listen(3000, ()=> {
-    console.log("Server is running");
+app.listen(port, () => {
+  console.log("Server is running");
 });
 
-// 404 page  
+// 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404'});
+  res.status(404).render("404", { title: "404" });
 });

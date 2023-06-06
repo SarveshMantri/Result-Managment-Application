35e415a2e4c124218e441d1fd4391ec6a12051e2
SarveshMantri <81086025+SarveshMantri@users.noreply.github.com>
2023-06-07 00:36:51 +0530

    Updated environment variable names

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 module.exports = mysql.createConnection({
-  host: process.env.AZURE_MYSQL_HOST || "localhost",
-  user: process.env.AZURE_MYSQL_USER || "root",
-  database: process.env.AZURE_MYSQL_DATABASE || "StudentDetails",
-  password: process.env.AZURE_MYSQL_PASSOWRD || "",
-  port: process.env.AZURE_MYSQL_PORT || 3306,
+  host: process.env.AWS_MYSQL_HOST || "localhost",
+  user: process.env.AWS_MYSQL_USER || "root",
+  database: process.env.AWS_MYSQL_DATABASE || "StudentDetails",
+  password: process.env.AWS_MYSQL_PASSOWRD || "",
+  port: process.env.AWS_MYSQL_PORT || 3306,
 });
